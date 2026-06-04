700a71e5beac69630d4a6a5709f6f2012aef728f
Nathaniel Ho <nxho93@gmail.com>
2026-06-04 03:57:36 -0400

    fix: vite dep optimization error due to circular import

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export * from './definitions';
 export * from './web';
+
+import { registerPlugin } from '@capacitor/core';
+import type { CheckoutPlugin } from './definitions';
+const Checkout = registerPlugin<CheckoutPlugin>('Checkout', {
+  web: () => import('./web').then((m) => new m.CheckoutWeb()),
+});
+
+export { Checkout };
--- src/web.ts
+++ src/web.ts
@@ -1,4 +1,4 @@
-import { registerPlugin, WebPlugin } from '@capacitor/core';
+import { WebPlugin } from '@capacitor/core';
 import type { CheckoutPlugin } from './definitions';
 
 export class CheckoutWeb extends WebPlugin implements CheckoutPlugin {
@@ -11,64 +11,59 @@ export class CheckoutWeb extends WebPlugin implements CheckoutPlugin {
     return options;
   }
 
+  async open(options: { key: string; amount: string }): Promise<{ response: string }> {
+    console.log(options);
 
-  async open(options: { key: string, amount: string }): Promise<{ response: string }> {
-    console.log(options)
-    
-    return new Promise((resolve,reject)=>{
-      var razorpay:any;
+    return new Promise((resolve, reject) => {
+      var razorpay: any;
       //add handlers to options to get the success response
-      var finalOps:any = options;
-  
-      finalOps['handler']=function(res:any){
+      var finalOps: any = options;
+
+      finalOps['handler'] = function (res: any) {
         console.log(res.razorpay_payment_id);
         resolve({
-          response:res
-        })
-        
+          response: res,
+        });
+      };
+
+      finalOps['modal.ondismiss'] = function () {
+        reject(JSON.stringify({ code: 2, description: 'Payment Canceled by User' }));
+      };
+
+      var retryCount = 0;
+      if (finalOps.hasOwnProperty('retry')) {
+        if (finalOps.retry.enabled === true) {
+          retryCount = finalOps.retry.max_count;
+        }
       }
 
-      finalOps['modal.ondismiss'] =function(){
-        reject(JSON.stringify({code:2,description:'Payment Canceled by User'}))
-      }
-      
-      var retryCount = 0;
-      if(finalOps.hasOwnProperty('retry')){
-          if(finalOps.retry.enabled===true){
-            retryCount = finalOps.retry.max_count;
-          }
-      }
-  
       //get the first script tag in the document
-      var rjs = document.getElementsByTagName('script')[0]
+      var rjs = document.getElementsByTagName('script')[0];
       //create a HTMLScriptElement object for rzp script injection
       var rzpjs = document.createElement('script');
-      rzpjs.id='rzp-jssdk';
-      rzpjs.setAttribute('src','https://checkout.razorpay.com/v1/checkout.js');
-      rjs.parentNode?.appendChild(rzpjs)
-      rzpjs.addEventListener('load',()=>{
-        try{
-            razorpay = new (window as any).Razorpay(finalOps)
-            razorpay.open()
-            razorpay.on(('payment.failed'),(res:any)=>{
-              retryCount = retryCount-1;
-              if(retryCount<0){
-                console.log(res);
-                rjs.parentNode?.removeChild(rzpjs);
-                reject(res.error);
-
-              }
-            })
-        }catch(err){
-           reject({
-             response:err})
+      rzpjs.id = 'rzp-jssdk';
+      rzpjs.setAttribute('src', 'https://checkout.razorpay.com/v1/checkout.js');
+      rjs.parentNode?.appendChild(rzpjs);
+      rzpjs.addEventListener('load', () => {
+        try {
+          razorpay = new (window as any).Razorpay(finalOps);
+          razorpay.open();
+          razorpay.on('payment.failed', (res: any) => {
+            retryCount = retryCount - 1;
+            if (retryCount < 0) {
+              console.log(res);
+              rjs.parentNode?.removeChild(rzpjs);
+              reject(res.error);
+            }
+          });
+        } catch (err) {
+          reject({
+            response: err,
+          });
         }
-      })
-    })
+      });
+    });
 
-    
-    
-    
     // var rjs = document.getElementsByTagName('script')[0]
     // var razorpay;
     // var js:HTMLScriptElement = document.createElement('script')
@@ -97,17 +92,5 @@ export class CheckoutWeb extends WebPlugin implements CheckoutPlugin {
     // return {
     //   response:options.key
     // }
-
-
   }
 }
-
-
-const Checkout = registerPlugin<CheckoutPlugin>('Checkout',{
-  web: () => import('./web').then(m => new m.CheckoutWeb())
-});
-
-export * from './definitions';
-export {Checkout}
-
-
