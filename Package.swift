3878b44018404aebc202651f84b35fa4e8ca7718
Nathaniel Ho <nxho93@gmail.com>
2026-07-22 23:51:25 -0400

    fix: actually add Razorpay dep

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/razorpay/razorpay-pod.git", from: "1.5.6")
     ],
     targets: [
         .target(
@@ -18,6 +19,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "RazorpayCheckout", package: "razorpay-pod")
             ],
             path: "ios/Sources/Checkout"),
         .testTarget(
